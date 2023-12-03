a40c077717b2a9a1211b9c6e710b02ba3ed6b521
ametsCS <carrera.amets@gmail.com>
2023-12-03 18:48:53 +0100

    bukatua

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-let users = [
-  {id: Date.now(), izena: "John", abizena: "Doe", email: "john@doe.com"},
-];
+const mongojs = require('mongojs');
+const db = mongojs('bezeroakdb', ['bezeroak']); 
+
+let users = [];
+
+db.bezeroak.find( function (err, userdocs){
+  if (err) {
+    console.log("Error: " + err);
+  } else {
+    users = userdocs;
+  }
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -20,19 +29,49 @@ router.get('/list', function(req, res, next) {
 
 router.post("/new", (req, res) => {
   users.push(req.body);
-  res.json(users);
+  // insert new user in mongo
+  db.bezeroak.insert(req.body, function (err, user){
+    if (err) {
+      console.log("Error: " + err);
+    } else {
+      console.log("Inserted: " + user);
+      res.json(user);
+    }
+  });
 });
+
 
 router.delete("/delete/:id", (req, res) => {
-  users = users.filter(user => user.id != req.params.id);
+  users = users.filter(user => user._id != req.params.id);
+  // remove user from mongo
+  db.bezeroak.remove({_id: mongojs.ObjectId(req.params.id)}, function (err, user){
+    if (err) {
+      console.log("Error: " + err);
+    } else {
+      console.log("Removed: " + user);
+    }
+  });
   res.json(users);
 });
 
+
 router.put("/update/:id", (req, res) => {
-  let user = users.find(user => user.id == req.params.id);
+  let user = users.find(user => user._id == req.params.id);
   user.izena = req.body.izena;
   user.abizena = req.body.abizena;
   user.email = req.body.email;
+
+  // update user in mongo
+  db.bezeroak.update({_id: mongojs.ObjectId(req.params.id) },
+  { $set: {izena: req.body.izena, abizena: req.body.abizena, email: req.body.email} },
+  function (err, user){
+    if (err) {
+      console.log("Error: " + err);
+    } else {
+      console.log("Updated: " + user);
+    }
+  });
+
   res.json(users);
 })
 
